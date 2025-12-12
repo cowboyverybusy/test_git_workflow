3c4a328b91eaa614125177f8abfe69776e31d336
shewenle <1281811451@qq.com>
2025-12-12 14:03:15 +0800

    fix

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("故事的小黄花? Hello World cowboy very busy!"))
+		w.Write([]byte("Hello World cowboy very busy!"))
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
